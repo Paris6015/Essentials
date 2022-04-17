b7dd2e96dded3ae719c6e055d3536c2a4cc24eeb
Paris Phan <parisphan1234@gmail.com>
2022-04-17 03:50:25 -0400

    Initial Commit

---
+++ Essentials/EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialsApp.swift
+//  Essentials
+//
+//  Created by Paris Phan on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
